d71bf1e24dcabc90221fe77ddce885460e999bcf
Aykut Yararbas <aykut_yararbas@bmc.com>
2016-12-21 23:43:26 -0800

    Google signin / draft

--- src/app/unprotected/signin/signin.component.ts
+++ src/app/unprotected/signin/signin.component.ts
@@ -4,6 +4,9 @@ import {User} from "../../shared/user.interface";
 import {AuthenticationService} from "../../shared/authentication.service";
 import {Router} from "@angular/router";
 
+declare var gapi:any;
+
+
 @Component({
   selector: 'app-signin',
   templateUrl: './signin.component.html',
@@ -23,12 +26,39 @@ export class SigninComponent implements OnInit {
    this.authenticationService.signin(this.myForm.value);
   }
 
+
+   onGoogleSigninSuccess=(googleUser: any)=>{
+     var profile = googleUser.getBasicProfile();
+     console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+     console.log('Name: ' + profile.getName());
+     console.log('Image URL: ' + profile.getImageUrl());
+     console.log('Email: ' + profile.getEmail());
+   }
+
+  onGoogleSigninFailure=(e)=>{
+    console.log("Google signin error");
+  }
+
+
+
+
   ngOnInit():any {
 
     this.myForm = this.fb.group({
       email: ['', Validators.required],
       password: ['', Validators.required],
     });
+
+    gapi.signin2.render('google-signin', {
+      'scope': 'profile email',
+      'width': 240,
+      'height': 50,
+      'longtitle': true,
+      'theme': 'dark',
+      'onsuccess': this.onGoogleSigninSuccess,
+      'onfailure': this.onGoogleSigninFailure
+    });
+
   }
 
 }
